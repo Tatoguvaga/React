b4cbb5a2626b893d6f0c4502d9a0b99173c24101
Gustavo <Gustavovarelas19@gmail.com>
2026-01-30 19:58:15 +0100

    async-await

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,24 +1,27 @@
+import type { GiphyRandomResponse, Gif } from "../data/giphy.response";
 
 
 const API_KEY = 'UFctt2RNEIa4MASKfIubVudqnrjq8eAp';
 
 
 
-const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`)
+const myRequest = fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+);
+
+const createImageInsideDOM = ( url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+};
 
 
 myRequest
 .then((response) => response.json())
-.then((data) => {
-
-    const imageUrl = data.data.images.original.url;
-    console.log(imageUrl);
-
-    const imgElement = document.createElement('img');
-    imgElement.src = imageUrl;
-
-    document.body.append(imgElement);
-
+.then(({ data }: GiphyRandomResponse) => {
+    const imageUrl = data.images.original.url;
+    createImageInsideDOM(imageUrl);
 }) 
 .catch ( err => {
     console.error(err);
---
+++ 01-reforzamiento/src/bases/11-async-await.ts
@@ -0,0 +1,26 @@
+
+import type { GiphyRandomResponse, Gif } from "../data/giphy.response";
+
+
+const API_KEY = 'UFctt2RNEIa4MASKfIubVudqnrjq8eAp';
+
+
+const createImageInsideDOM = ( url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+};
+
+const getRandomGifUrl = async (): Promise<string> => {
+
+    const response = await fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+);
+
+    const { data }: GiphyRandomResponse = await response.json();
+
+    return data.images.original.url;
+};
+
+getRandomGifUrl().then(createImageInsideDOM);
