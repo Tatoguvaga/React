b0bbffa6111b6c13d4ec8b136c11f1fff13f4816
Gustavo <Gustavovarelas19@gmail.com>
2026-02-07 19:48:54 +0100

    impresion-variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,10 +1,28 @@
 
-export function MyAwesomeApp() {
+export const MyAwesomeApp = () => {
+
+    const firstName = 'Gustavo';
+    const lastName = 'Varelas';
+
+    const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+    const isActive = false;
+
+    const address = {
+        zipCode: 'ABC-123',
+        country: 'España',
+    };
 
     return (
         <>
-            <h1>Gustavo</h1>
-            <h3>Varelas</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{2 + 2}</p>
+
+            <h1>{isActive ? 'Activo' : 'No Activo'}</h1>
+
+            <p>{JSON.stringify(address)}</p>
         </>
     );
 
