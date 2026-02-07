4e9f58b143f3db243f475ad0bb8848c1eb8b1883
Gustavo <Gustavovarelas19@gmail.com>
2026-02-07 21:58:37 +0100

    componentes-counter

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,17 +1,15 @@
+import { ItemCounter } from "./shopping-cart/ItemCounter";
 
 
 export function FirstStepsApp() {
 
     return (
         <>
-            <h1>Hola Mundo!!</h1>
-            <p>Esto es un parrafo!!</p>
-
-            <button>Click me</button>
-
-            <div>
-                <h2>Hola dentro de un div</h2>
-            </div>
+            <h1>Carrito de Compras</h1>
+            
+            <ItemCounter/>
+            <ItemCounter/>
+            <ItemCounter/>
         </>
     );
 
