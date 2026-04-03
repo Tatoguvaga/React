e545ecc55eef44486fe1d5aa86cd066696a45cea
Gustavo <Gustavovarelas19@gmail.com>
2026-04-03 18:37:31 +0200

    esperar_argumentos_especificos

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,14 +1,30 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 
+const mockItemCounter = vi.fn((_props: unknown) => {
+    return (<div data-testid="ItemCounter" />)
+});
 
 vi.mock('./shopping-cart/ItemCounter', () => ({
-    ItemCounter: () => <div data-testid="ItemCounter" />,
+    ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
+
+// vi.mock('./shopping-cart/ItemCounter', () => ({
+//     ItemCounter: (props: unknown) => (
+//     <div 
+//     data-testid="ItemCounter" 
+//     name={props.name} 
+//     quantity={props.quantityu} />
+//     ),
+// }));
 
 
 describe('FirstStepsApp', () => {
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
 
     test('should match snapshot', () => {
 
@@ -18,15 +34,28 @@ describe('FirstStepsApp', () => {
     });
 
     test('should render the correct number of ItemCounter components', () => {
-
         render(<FirstStepsApp />);
 
         const itemCounters = screen.getAllByTestId('ItemCounter');
 
         expect(itemCounters.length).toBe(3);
-
-        screen.debug();
     });
 
+    test('should render ItemCounter with correct props', () => {
+        render(<FirstStepsApp />)
 
+        expect(mockItemCounter).toHaveBeenCalledTimes(3);
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Nintendo Switch 2', 
+            quantity: 1,
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Pro Controller', 
+            quantity: 2,
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Super Smash', 
+            quantity: 5,
+        });
+    })
 });
